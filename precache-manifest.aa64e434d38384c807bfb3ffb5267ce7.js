ddf96147c521c5b521d747f7b7cc8c9c633bbb44
Urvashi Indu <urvashiindu09@gmail.com>
2023-06-20 23:04:38 +0530

    Updates

---
+++ precache-manifest.aa64e434d38384c807bfb3ffb5267ce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "514250765b0dea3439d0",
+    "url": "/Age_Calculator/static/css/main.5f4a29d2.chunk.css"
+  },
+  {
+    "revision": "514250765b0dea3439d0",
+    "url": "/Age_Calculator/static/js/main.d54681a2.chunk.js"
+  },
+  {
+    "revision": "c86c01bf95ee97c1319c",
+    "url": "/Age_Calculator/static/js/runtime~main.5d75a68b.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/Age_Calculator/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "f54d1ac797f637aac8ec",
+    "url": "/Age_Calculator/static/js/3.b359b5d6.chunk.js"
+  },
+  {
+    "revision": "62296549ff7817222250c337a07dbfa7",
+    "url": "/Age_Calculator/index.html"
+  }
+];
